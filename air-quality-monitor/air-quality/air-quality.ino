6644936c04e83e93e3ea8336759a4c38f218990c
Sankara Rameswaran <hello@sankara.me>
2020-08-06 22:05:39 -0700

    Adding temperature & humidity sensors

--- air-quality-monitor/air-quality/air-quality.ino
+++ air-quality-monitor/air-quality/air-quality.ino
@@ -1,22 +1,33 @@
+#define WIFI_COMPATIBLE_BOARD 1
+
 #include <Arduino.h>
 #include <SSD1306Ascii.h>
 #include <SSD1306AsciiWire.h>
 #include <SdsDustSensor.h>
+#include <DHT.h>
+
+#ifdef WIFI_COMPATIBLE_BOARD
 #include "properties.h"
+#endif
+
 
 
 //R, G, B
-int GREEN[] = {0x00, 0xff, 0x00};
-int YELLOW[] = {0x00, 0x00, 0xff};//Yellow isn't very visible hence the choice of blue
-int RED[] = {0xff, 0x00, 0x00};
+int GREEN[] = {0x00, 0xf, 0x00};
+int YELLOW[] = {0x00, 0x00, 0xf};//Yellow isn't very visible hence the choice of blue
+int RED[] = {0xf, 0x00, 0x00};
 
 //LED module pins
 int pinLEDR = 6;
 int pinLEDG = 5;
 int pinLEDB = 3;
 
+//DHT Sensor Pin
+int pinDHT = 8;
+
 SdsDustSensor sds(Serial1);//Serial1 is Hardware Rx/Tx on the nano
 SSD1306AsciiWire screen;
+DHT dht(pinDHT, DHT11);
 
 char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
@@ -35,25 +46,43 @@ int* getPmAcceptability(PmResult pm) {
   }
 }
 
-void showResults(int* status, PmResult pm) {
+void displayOnScreen(PmResult pm, float humidity, float temperature) {
   char pm25_s[12];
   dtostrf(pm.pm25, 7, 2, pm25_s);
   char pm10_s[12];
   dtostrf(pm.pm10, 7, 2, pm10_s);
+  char h_s[12];
+  dtostrf(humidity, 2, 0, h_s);
+  char t_s[12];
+  dtostrf(temperature, 7, 1, t_s);
 
+  //Flickers
+  screen.clear();
+
+  screen.setCursor(0, 0);
+  screen.print(h_s);
+  screen.print(F("% "));
+  screen.print(t_s);
+  screen.print("  ");
+  screen.print((char)248);
+  screen.println("C");
+
+  screen.println();
+
+  screen.print(F("PM 2.5 : "));
+  screen.println(pm25_s);
+  screen.print(F("PM 10  : "));
+  screen.println(pm10_s);
+}
+
+void showResults(int* status, PmResult pm, float humidity, float temperature) {
   //LED Output
   analogWrite(pinLEDR, status[0]);
   analogWrite(pinLEDG, status[1]);
   analogWrite(pinLEDB, status[2]);
 
   //OLED Output
-  screen.setCursor(0, 0);
-  screen.print(F("PM 2.5 : "));
-  screen.println(pm25_s);
-  screen.println();
-  screen.println();
-  screen.print(F("PM 10  : "));
-  screen.println(pm10_s);
+  displayOnScreen(pm, humidity, temperature);
 }
 
 void initDisplay() {
@@ -83,6 +112,11 @@ void initIOT() {
   ArduinoCloud.printDebugInfo();  
 }
 
+void initDHT() {
+  Serial.println("Initializing DHT");
+  dht.begin();
+}
+
 void setup() {
   Serial.begin(9600);
   delay(1500);
@@ -90,24 +124,32 @@ void setup() {
 
   initDisplay();
   initSDS();
+  initDHT();
+#ifdef WIFI_COMPATIBLE_BOARD
   initIOT();
+#endif
 }
 
 void loop() {
   delay(2000);
 
   PmResult pm = sds.readPm();
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+  
   if (pm.isOk()) {
     Serial.print("PM2.5 = ");
     Serial.println(String(pm.pm25));
     Serial.print(", PM10 = ");
     Serial.println(String(pm.pm10));
     int *statusLight = getPmAcceptability(pm);
-    showResults(statusLight, pm);
+    showResults(statusLight, pm, humidity, temperature);
 
     pm10 = pm.pm10;
     pm25 = pm.pm25;
+#ifdef WIFI_COMPATIBLE_BOARD
     ArduinoCloud.update();
+#endif
   } else {
     Serial.print("Could not read values from sensor, reason: ");
     Serial.println(pm.statusToString());
--- air-quality-monitor/air-quality/properties.h
+++ air-quality-monitor/air-quality/properties.h
@@ -4,14 +4,16 @@
 
 float pm10;
 float pm25;
+CloudTemperature temperature;
+float humidity;
 
 void initProperties(){
 
   ArduinoCloud.setThingId(THING_ID);
   ArduinoCloud.addProperty(pm10, READ, 1 * SECONDS, NULL);
   ArduinoCloud.addProperty(pm25, READ, 1 * SECONDS, NULL);
-
+  ArduinoCloud.addProperty(temperature, READ, ON_CHANGE, NULL, 0.5);
+  ArduinoCloud.addProperty(humidity, READ, ON_CHANGE, NULL, 1);
 }
 
 WiFiConnectionHandler ArduinoIoTPreferredConnection(SSID, PASS);
-
