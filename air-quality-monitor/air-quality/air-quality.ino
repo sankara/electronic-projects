46b5571d9f4cb35505876d2793fe31206de97e09
Sankara Rameswaran <hello@sankara.me>
2020-08-30 21:15:13 -0700

    Fixing fixing fixing

--- air-quality-monitor/air-quality/air-quality.ino
+++ air-quality-monitor/air-quality/air-quality.ino
@@ -1,5 +1,5 @@
 #define WIFI_COMPATIBLE_BOARD 1
-
+#define AQM_DEBUG 0
 #include <Arduino.h>
 #include <SSD1306Ascii.h>
 #include <SSD1306AsciiWire.h>
@@ -8,10 +8,13 @@
 #include <fonts/Callibri15.h>
 #include <fonts/System5x7.h>
 
-#ifdef WIFI_COMPATIBLE_BOARD
-#include "properties.h"
-#endif
 
+#if WIFI_COMPATIBLE_BOARD
+#include "properties.h"
+//Fails with EEPROM issue. Edit .h manualy to disable cache
+//#include <ezTime.h>
+//Timezone CA_tz;
+#endif
 
 
 //R, G, B
@@ -29,133 +32,141 @@ int pinDHT = 8;
 
 SdsDustSensor sds(Serial1);//Serial1 is Hardware Rx/Tx on the nano
 SSD1306AsciiWire screen;
-DHT dht(pinDHT, DHT11);
+DHT dht;
 
-char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
-  char fmt[20];
-  sprintf(fmt, "%%%d.%df", width, prec);
-  sprintf(sout, fmt, val);
-  return sout;
+char *dtostrf(double val, signed char width, unsigned char prec, char *sout) {
+    char fmt[20];
+    sprintf(fmt, "%%%d.%df", width, prec);
+    sprintf(sout, fmt, val);
+    return sout;
 }
 
-int* getPmAcceptability(PmResult pm) {
-  if (pm.pm25 > 0 && pm.pm25 < 12) {
-    return GREEN;
-  } else if (pm.pm25 >= 12 && pm.pm25 < 35) {
-    return YELLOW;
-  } else if (pm.pm25 >= 35) {
-    return RED;
-  }
+int *getPmAcceptability(PmResult pm) {
+    if (pm.pm25 > 0 && pm.pm25 < 12) {
+        return GREEN;
+    } else if (pm.pm25 >= 12 && pm.pm25 < 35) {
+        return YELLOW;
+    } else if (pm.pm25 >= 35) {
+        return RED;
+    }
 }
 
 void displayOnScreen(PmResult pm, float humidity, float temperature) {
-  char pm25_s[12];
-  dtostrf(pm.pm25, 7, 2, pm25_s);
-  char pm10_s[12];
-  dtostrf(pm.pm10, 7, 2, pm10_s);
-  char h_s[12];
-  dtostrf(humidity, 2, 0, h_s);
-  char t_s[12];
-  dtostrf(temperature, 7, 1, t_s);
+    char pm25_s[12];
+    dtostrf(pm.pm25, 7, 2, pm25_s);
+    char pm10_s[12];
+    dtostrf(pm.pm10, 7, 2, pm10_s);
+    char h_s[12];
+    dtostrf(humidity, 2, 0, h_s);
+    char t_s[12];
+    dtostrf(temperature, 7, 1, t_s);
 
-  //Flickers
-  screen.clear();
+    //Flickers
+    screen.clear();
 
-  screen.setFont(System5x7);
-  screen.setCursor(0, 0);
-  screen.print(h_s);
-  screen.print(F("% "));
-  screen.print("    ");
-  screen.print(t_s);
-  screen.print((char)128);
-  screen.println("C");
+    screen.setFont(System5x7);
+    screen.setCursor(0, 0);
+    screen.print(h_s);
+    screen.print(F("% "));
+    screen.print(F("    "));
+    screen.print(t_s);
+    screen.print((char) 128);
+    screen.println(F("C"));
 
-  screen.println();
+    screen.println();
 
-  screen.setFont(Callibri15);
-  screen.print(F("  PM 2.5 : "));
-  screen.println(pm25_s);
-  screen.print(F("  PM 10  : "));
-  screen.println(pm10_s);
+    screen.setFont(Callibri15);
+    screen.print(F("  PM 2.5 : "));
+    screen.println(pm25_s);
+    screen.print(F("  PM 10  : "));
+    screen.println(pm10_s);
 }
 
-void showResults(int* status, PmResult pm, float humidity, float temperature) {
-  //LED Output
-  analogWrite(pinLEDR, status[0]);
-  analogWrite(pinLEDG, status[1]);
-  analogWrite(pinLEDB, status[2]);
+void showResults(int *status, PmResult pm, float humidity, float temperature) {
+    //LED Output
+    analogWrite(pinLEDR, status[0]);
+    analogWrite(pinLEDG, status[1]);
+    analogWrite(pinLEDB, status[2]);
 
-  //OLED Output
-  displayOnScreen(pm, humidity, temperature);
+    //OLED Output
+    displayOnScreen(pm, humidity, temperature);
 }
 
 void initDisplay() {
-  Wire.begin();
-  Wire.setClock(400000L);
+    Wire.begin();
+    Wire.setClock(400000L);
 
-  screen.begin(&Adafruit128x64, 0x3C);
+    screen.begin(&Adafruit128x64, 0x3C);
 
-  screen.setFont(Callibri15);
-  screen.clear();
+    screen.setFont(Callibri15);
+    screen.clear();
 
-  screen.println(F("Starting..."));
-  delay(3000);
+    screen.println(F("Starting..."));
+    delay(3000);
 }
 
 void initSDS() {
-  sds.begin();
-  Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
-  Serial.println(sds.setActiveReportingMode().toString()); // ensures sensor is in 'active' reporting mode
-  //Serial.println(sds.setContinuousWorkingPeriod().toString());
+    sds.begin();
+    Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
+    Serial.println(sds.setActiveReportingMode().toString()); // ensures sensor is in 'active' reporting mode
+    //Serial.println(sds.setContinuousWorkingPeriod().toString());
 }
 
 void initIOT() {
-  initProperties();
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();  
+    initProperties();
+    ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+    setDebugMessageLevel(2);
+    ArduinoCloud.printDebugInfo();
 }
 
 void initDHT() {
-  Serial.println("Initializing DHT");
-  dht.begin();
+    Serial.println("Initializing DHT");
+    dht.setup(pinDHT);
+}
+
+void initNTP(){
+    //CA_tz.setLocation("Pacific/Los_Angeles");
 }
 
 void setup() {
-  Serial.begin(9600);
-  delay(1500);
-  Serial.println("initializing");
+    Serial.begin(9600);
+    delay(1500);
+    Serial.println("initializing");
 
-  initDisplay();
-  initSDS();
-  initDHT();
-#ifdef WIFI_COMPATIBLE_BOARD
-  initIOT();
+    initDisplay();
+    initSDS();
+    initDHT();
+#if WIFI_COMPATIBLE_BOARD
+    initIOT();
+    //initNTP();
 #endif
 }
 
 void loop() {
-  delay(2000);
+    delay(2000);
 
-  PmResult pm = sds.readPm();
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
-  
-  if (pm.isOk()) {
-    Serial.print("PM2.5 = ");
-    Serial.println(String(pm.pm25));
-    Serial.print(", PM10 = ");
-    Serial.println(String(pm.pm10));
-    int *statusLight = getPmAcceptability(pm);
-    showResults(statusLight, pm, humidity, temperature);
+    PmResult pm = sds.readPm();
+    humidity = dht.getHumidity();
+    temperature = dht.getTemperature();
 
-    pm10 = pm.pm10;
-    pm25 = pm.pm25;
-#ifdef WIFI_COMPATIBLE_BOARD
-    ArduinoCloud.update();
+    if (pm.isOk()) {
+#if AQM_DEBUG
+        Serial.print("PM2.5 = ");
+        Serial.println(String(pm.pm25));
+        Serial.print(", PM10 = ");
+        Serial.println(String(pm.pm10));
 #endif
-  } else {
-    Serial.print("Could not read values from sensor, reason: ");
-    Serial.println(pm.statusToString());
-  }
+        int *statusLight = getPmAcceptability(pm);
+        showResults(statusLight, pm, humidity, temperature);
+
+        pm10 = pm.pm10;
+        pm25 = pm.pm25;
+#if WIFI_COMPATIBLE_BOARD
+        ArduinoCloud.update();
+        //Serial.println("Current time: " + CA_tz.dateTime());
+#endif
+    } else {
+        Serial.print(F("Could not read values from sensor, reason: "));
+        Serial.println(pm.statusToString());
+    }
 }
